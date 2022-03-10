8da1fe49c9ee7ddbd975b6040aea45389d545370
dennismuturi <dennismuturi1@gmail.com>
2022-03-10 18:47:00 +0000

    Modified- Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-int main()
-{
-	puts("Programming is like building a multilingual puzzle");
 
-return 0;
+// The main entry of the program
+
+/**
+ * has a method to prints a string 
+ * 
+ */
+
+int main(voidi)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
 
