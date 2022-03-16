77d9be4c78adad3c87b79614d3d4ce5c8a1ae2c1
dennismuturi <dennismuturi1@gmail.com>
2022-03-16 08:21:43 +0000

    a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+int _abs(int)
+{	
+	if (int > 0)
+	{
+		_putchar(int);
+		return(int);
+	}
+	else 
+	{
+		int multiply=int * 2;
+		int result=int - multiply;
+	        return(int);	
+	
+	}
+
+
+
+
+	return(0);
+
+}
