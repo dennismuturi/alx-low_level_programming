db34cdfcae373c6daf2b336b75fb9ccd356b9c0f
dennismuturi <dennismuturi1@gmail.com>
2022-03-16 07:48:58 +0000

    Modified:function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,23 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
      		_putchar('+');
         	_putchar('\n');
 		return(1);
+	}
 	else if (n < 0)
+	{
 		_putchar('-');
 	        _putchar('\n');
 		return(-1);
+	}
 	else
+	{	
 		_putchar('0');
 	        _putchar('\n');
 	         return(0)
+	}
 
 	return(0);
 }
