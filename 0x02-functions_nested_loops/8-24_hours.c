2fcd8447c586b51734e80fc18184be9e50c269f5
dennismuturi <dennismuturi1@gmail.com>
2022-03-16 09:24:39 +0000

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+void jack_bauer(void)
+{
+	int hr=00;
+	int min;
+	for (min = 00 ;min <= 60;min++)
+	{	
+		min=min + 1;
+		if (min == 60)
+		{
+			hr=hr + 1;
+			min=00;
+		
+		}
+
+		_putchar(hr);
+		_putchar(':');
+		_putchar(min);
+		-putchar('\n');
+
+	}
+
+}
