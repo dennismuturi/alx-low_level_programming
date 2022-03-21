c1792add75545c355b5c20126a49327a96ea6cd2
Dennis Muturi <dennismuturi1@gmail.com>
2022-03-21 11:15:37 -0700

    Modified: reset_to_96 function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
-/*
- * function reset_to_98: changes value to 98 using a pointer
- * @*n - a pointer to an int
+/**
+ * reset_to_98 - changes value to 98 using a pointer
+ * @n: a pointer to an int
  */
 
 void reset_to_98(int *n)
