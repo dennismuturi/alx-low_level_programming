2910a247c3a023e96a1fb861f2c1679b8a5b59aa
Dennis Muturi <dennismuturi1@gmail.com>
2022-03-22 03:46:36 -0700

    Modified:a function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,10 +5,11 @@
  * Return: the length of a string
  */
 int _strlen(char *s)
-{
+{	char str_arr[] = *s;
+	int i;
 	int str_len = 0;
 
-	while (*s != '\0')
+	for (i = 0; str_arr[i] != '\0'; i++)
 	{
 		str_len++;
 	}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,8 @@
+/**
+ * print_rev - function prints a string in reverse, followed by a newline
+ * @s: a string
+ */
+void print_rev(char *s)
+{
+
+}
