e40913267c2aecf2ef8f241371db5aa420832c88
Dennis Muturi <dennismuturi1@gmail.com>
2022-03-22 06:36:07 -0700

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: a string
+ */
+void rev_string(char *s)
+{
+
+	int i = 0;
+	int l;
+	char rev;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (l = i - 1; l >= 0; l--)
+	{
+		rev = s[l];
+		_putchar(rev);
+	}
+}
