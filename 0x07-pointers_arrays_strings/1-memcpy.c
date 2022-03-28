62fbd61d110ce651e7c658b79cb0c45175536522
Dennis Muturi <dennismuturi1@gmail.com>
2022-03-28 16:49:44 -0700

    a function theat copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: desination
+ * @src: source
+ * @n: number of bytes
+ * Return: destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	
+	while (i < n}
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
