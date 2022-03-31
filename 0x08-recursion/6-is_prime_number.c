d8cee3a0235de58aab5f8ecc1004c7e44c777ebe
Dennis Muturi <dennismuturi1@gmail.com>
2022-03-31 03:43:35 -0700

    is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,15 @@ int is_prime_number(int n)
 {
 	int i = 2;
 
-	if ((n % i) == 0)
+	if (n == 1)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (0);
+	}
+	else if ((n % i) == 0)
 	{
 		return (0);
 	}
