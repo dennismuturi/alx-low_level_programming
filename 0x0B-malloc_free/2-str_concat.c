416ccac16760b47d42097389fca79bba0e6a2af5
Dennis Muturi <dennismuturi1@gmail.com>
2022-04-04 07:27:40 -0700

    Modified

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,52 @@
-#include "main.h"
 #include <stdlib.h>
+
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
+ * str_concat - concatenates two string
  *
- * @str: The string to copy
+ * @s1: the first string
+ * @s2: the string to add to @s1
  *
- * Return: a pointer to the duplicated string, NULL if insufficient memory
- * or if @str is NULL
+ * Return: a pointer that points to a newly allocated space which
+ * contains the contents of @s1, followed by the contents of @s2,
+ * and null terminated. Should return NULL on failure
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *ar;
+	char *s3;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
+	unsigned int s3len;
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[s1len])
+		s1len++;
+	while (s2[s2len])
+		s2len++;
+
+	s3len = s1len + s2len;
+
+	s3 = malloc(sizeof(char) * s3len + 1);
+	if (s3 == NULL)
 		return (NULL);
 
-	while (str[i])
-		i++;
-
-	ar = malloc(sizeof(char) * (i + 1));
-
-	if (ar == NULL)
-		return (NULL);
-
-	while (str[j])
+	while (i < s1len)
 	{
-		ar[j] = str[j];
-		j++;
+		s3[i] = s1[i];
+		i++;
 	}
 
-	ar[j + 1] = 0;
-	return (ar);
+	while (i <= s3len)
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (s3);
 }
